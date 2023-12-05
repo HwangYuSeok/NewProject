8e9589b87323b6ee2acc816d6443788b4bf3d982
HwangYuSeok <152045672+HwangYuSeok@users.noreply.github.com>
2023-12-05 23:35:54 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          안녕하세요 접니다
+        </p>
+        <p>
+          학번은 20201896입니다<br/>
+          그럼 이만
+        </p>
+        
+
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
